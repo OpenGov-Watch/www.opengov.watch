c9041916611de8c6a64c7f49dc9faeb706c3aece
Tommi Enenkel <tommi.enenkel@gmail.com>
2024-02-14 14:01:46 +0100

    hide blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          //{to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://www.opengov.watch/',
             label: 'GitHub',
